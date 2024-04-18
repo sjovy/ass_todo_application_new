edfcf276bfe9d8f0ef749bae3d42644920edb74d
sjovy <thomas.sjovy@gmail.com>
2024-04-18 08:08:16 +0200

    simplified todoitemdao even further. this general strategy could also be used in other classes where many filters follow the same logic.

--- src/main/java/se/lexicon/dao/TodoItemDAO.java
+++ src/main/java/se/lexicon/dao/TodoItemDAO.java
@@ -13,7 +13,10 @@ public interface TodoItemDAO {
     List<TodoItem> findAll();
     List <TodoItem> findAllByDoneStatus(boolean done);
     List <TodoItem> findByTitleContains(String title);
-    // List <TodoItem> find(Predicate<TodoItem> filter);
+
+    List<TodoItem> findMany(Predicate<TodoItem> condition);
+    // List <TodoItem> findOne(Predicate<TodoItem> filter);
+
     List <TodoItem> findByPersonId(int personId);
     List <TodoItem> findByDeadlineBefore(LocalDate deadline);
     List <TodoItem> findByDeadlineAfter(LocalDate deadline);
--- src/main/java/se/lexicon/dao/TodoItemDAOCollection.java
+++ src/main/java/se/lexicon/dao/TodoItemDAOCollection.java
@@ -17,94 +17,54 @@ public class TodoItemDAOCollection implements TodoItemDAO {
     }
 
     @Override
-    public TodoItem findById(int id) {
-        return todoItemsList.stream()
-                            .filter(tdi -> tdi.getId() == id)
-                            .findFirst()
-                            .orElse(null);
-    }
-
-    @Override
     public List<TodoItem> findAll() {
         return Collections.unmodifiableList(todoItemsList);
-    }
-
-    @Override
-    public List<TodoItem> findAllByDoneStatus(boolean done) {
-        return todoItemsList.stream()
-                            .filter(tdi -> tdi.isDone() == done)
-                            .collect(Collectors.toList());
-    }
-
-    @Override
-    public List<TodoItem> findByTitleContains(String title) {
-        return todoItemsList.stream()
-                            .filter(tdi -> tdi.getTitle().contains(title))
-                            .collect(Collectors.toList());
-    }
-
-    /*@Override
-    public List<TodoItem> find(Predicate<TodoItem> filter) {
-
-        // Set up a list to hold the todoItems based on the title
-        List<TodoItem> outputList = new ArrayList<>();
-
-        for (TodoItem tdi : todoItemsList) {
-            if (filter.test(tdi)) {
-                outputList.add(tdi);
-            }
-        }
-        return outputList;
-        // return todoItemsList.stream().filter(tdi -> tdi.getTitle().contains(title)).toList();
-    }*/
-
-    @Override
-    public List<TodoItem> findByPersonId(int personId) {
-
-        // Set up a list to hold the todoItems based on the creator's ID
-        List<TodoItem> outputList = new ArrayList<>();
-
-        for (TodoItem tdi : todoItemsList) {
-            Person todoItemCreator = tdi.getCreator();
-            if (todoItemCreator.getPersonId() == personId) {
-                outputList.add(tdi);
-            }
-        }
-        return outputList;
-    }
-
-    @Override
-    public List<TodoItem> findByDeadlineBefore(LocalDate deadline) {
-
-        // Set up a list to hold the todoItems based on the deadline
-        List<TodoItem> outputList = new ArrayList<>();
-
-        for (TodoItem tdi : todoItemsList) {
-            LocalDate todoItemDeadline = tdi.getDeadline();
-            if (todoItemDeadline.isBefore(deadline)) {
-                outputList.add(tdi);
-            }
-        }
-        return outputList;
-    }
-
-    @Override
-    public List<TodoItem> findByDeadlineAfter(LocalDate deadline) {
-
-        // Set up a list to hold the todoItems based on the deadline
-        List<TodoItem> outputList = new ArrayList<>();
-
-        for (TodoItem tdi : todoItemsList) {
-            LocalDate todoItemDeadline = tdi.getDeadline();
-            if (todoItemDeadline.isAfter(deadline)) {
-                outputList.add(tdi);
-            }
-        }
-        return outputList;
     }
 
     @Override
     public void remove(TodoItem todoItem) {
         todoItemsList.removeIf(p -> p.equals(todoItem));
     }
+
+    @Override
+    public TodoItem findById(int id) {
+        return todoItemsList.stream()
+                .filter(tdi -> tdi.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+
+    @Override
+    public List<TodoItem> findMany(Predicate<TodoItem> condition) {
+        return todoItemsList.stream()
+                            .filter(condition)
+                            .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<TodoItem> findByPersonId(int personId) {
+        return findMany(tdi -> tdi.getCreator().getPersonId() == personId);
+    }
+
+    @Override
+    public List<TodoItem> findByDeadlineBefore(LocalDate deadline) {
+        return findMany(tdi -> tdi.getDeadline().isBefore(deadline));
+    }
+
+    @Override
+    public List<TodoItem> findByDeadlineAfter(LocalDate deadline) {
+        return findMany(tdi -> tdi.getDeadline().isAfter(deadline));
+    }
+
+    @Override
+    public List<TodoItem> findAllByDoneStatus(boolean done) {
+        return findMany(tdi -> tdi.isDone() == done);
+    }
+
+    @Override
+    public List<TodoItem> findByTitleContains(String title) {
+        return findMany(tdi -> tdi.getTitle().contains(title));
+    }
+
+
 }
